985f7452872c3d0e74a7bc5e7fde93889fbe5cd4
chaeeun-seo <seochaeeun961126@gmail.com>
2023-10-09 23:11:11 +0900

    import CustomButton and add CustomButton to navigate to Customizer page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 
 import state  from '../store';
+import CustomButton from '../components';
 import {
     headContainerAnimation,
     headContentAnimation,
@@ -30,6 +31,13 @@ const Home = () => {
                         <p className='max-w-lg font-normal text-gray-600 text-base'>
                             Create your unique and exclusive t-shirt with ifland studio's 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style with the help of AI.
                         </p>
+
+                        <CustomButton 
+                            type="filled"
+                            title="Customize it"
+                            handleClick={() => state.intro = false}
+                            customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                        />
                     </motion.div>
                 </motion.div>
             </motion.section>
