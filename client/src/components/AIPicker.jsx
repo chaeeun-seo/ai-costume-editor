34d759c3cc223bc15c60c62e035368a4a597e465
chaeeun-seo <seochaeeun961126@gmail.com>
2023-10-10 19:58:03 +0900

    create buttons to submit the prompt in AIPicker

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -12,6 +12,30 @@ const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
         onChange={(e) => setPrompt(e.target.value)} 
         className="aipicker-textarea"
       />
+      <div className='flex flex-wrap gap-3'>
+        {generatingImg ? (
+          <CustomButton 
+            type="outline"
+            title="Asking AI..."
+            customStyles="text-xs"
+          />
+        ) : (
+          <>
+            <CustomButton 
+              type="outline"
+              title="AI Logo"
+              handleClick={() => handleSubmit('logo')}
+              customStyles="text-xs"
+            />
+            <CustomButton 
+              type="filled"
+              title="AI Full"
+              handleClick={() => handleSubmit('full')}
+              customStyles="text-xs"
+            />
+          </>
+        )}
+      </div>
     </div>
   )
 }
