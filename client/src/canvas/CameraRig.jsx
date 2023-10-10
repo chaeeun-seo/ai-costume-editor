ec3a3c28a346502e580846e31cbb2869f12ccf30
chaeeun-seo <seochaeeun961126@gmail.com>
2023-10-10 21:01:27 +0900

    change the values to make shirt look great

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -16,11 +16,11 @@ const CameraRig = ({ children }) => {
         // set the initial position of the model
         let targetPosition = [-0.4, 0, 2];
         if(snap.intro) {
-            if (isBreakpoint) targetPosition = [0, 0, 0.7];
-            if (isMobile) targetPosition = [0, 0.2, 0.7];
+            if (isBreakpoint) targetPosition = [0, 0, 2];
+            if (isMobile) targetPosition = [0, 0.2, 2.5];
         } else {
-            if (isMobile) targetPosition = [0, 0, 0.7];
-            else targetPosition = [0, 0, 0.7];
+            if (isMobile) targetPosition = [0, 0, 2.5];
+            else targetPosition = [0, 0, 2];
         }
 
         // set model camera position
@@ -29,7 +29,7 @@ const CameraRig = ({ children }) => {
         // set the model rotation smoothly
         easing.dampE(
             group.current.rotation,
-            [state.pointer.y , -state.pointer.x * 3 , 0],
+            [state.pointer.y / 3 , -state.pointer.x , 0],
             0.25, 
             delta,
         )
