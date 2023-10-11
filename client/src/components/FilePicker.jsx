e50728a30475a1ed5b951ab9bfe78721f0f6bd1f
chaeeun-seo <seochaeeun961126@gmail.com>
2023-10-11 15:07:21 +0900

    make a FilePicker that is used to upload images and preview uploaded image and apply the image to logo or full texture

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const FilePicker = () => {
+import CustomButton from './CustomButton';
+
+const FilePicker = ({ file, setFile, readFile }) => {
+  const [imgSrc, setImgSrc] = useState("");
+
   return (
-    <div>FilePicker</div>
+    <div className='filepicker-container'>
+      <div className='flex-1 flex flex-col gap-2'>
+        <input 
+          id='file-upload'
+          type="file" 
+          accept='image/*'
+          onChange={(e) => {
+            const reader = new FileReader();
+
+            setFile(e.target.files[0]);
+            reader.readAsDataURL(e.target.files[0])
+
+            reader.onloadend = () => {
+              setImgSrc(reader.result);
+            }
+          }}
+          className= "w-full block"
+        />
+        <label htmlFor="file-upload" className='filepicker-label text-center'>
+          Upload File
+        </label>
+
+        <p className='mt-2 text-gray-600 text-xs truncate'>
+          {file === '' ? "No file selected" : file.name}
+        </p>
+        <img src={imgSrc} alt="" />
+      </div>
+
+      <div className='mt-4 flex flex-wrap gap-3'>
+        <CustomButton 
+          type="outline"
+          title="Logo"
+          handleClick={() => readFile('logo')}
+          customStyles='text-xs'
+        />
+        <CustomButton 
+          type="filled"
+          title="Full"
+          handleClick={() => readFile('full')}
+          customStyles='text-xs'
+        />
+      </div>
+    </div>
   )
 }
 
