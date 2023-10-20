64e4239356ebb6a1d5f6f42e6601b941273a24b6
chaeeun-seo <seochaeeun961126@gmail.com>
2023-10-20 18:49:33 +0900

    add DallePicker to index.js in components folder to export

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,8 @@
 import CustomButton from "./CustomButton";
 import AIPicker from "./AIPicker";
+import DallePicker from "./DallePicker";
 import ColorPicker from "./ColorPicker";
 import FilePicker from "./FilePicker";
 import Tab from "./Tab";
 
-export { CustomButton, AIPicker, ColorPicker, FilePicker, Tab, };
+export { CustomButton, AIPicker, DallePicker, ColorPicker, FilePicker, Tab, };
