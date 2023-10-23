2b9698e1f8c0d3d3886e72f41cbe139e0ddd8626
chaeeun-seo <seochaeeun961126@gmail.com>
2023-10-23 19:14:00 +0900

    add SDPicker to export in index.js in components folder

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,8 +1,9 @@
 import CustomButton from "./CustomButton";
 import AIPicker from "./AIPicker";
 import DallePicker from "./DallePicker";
+import SDPicker from "./SDPicker";
 import ColorPicker from "./ColorPicker";
 import FilePicker from "./FilePicker";
 import Tab from "./Tab";
 
-export { CustomButton, AIPicker, DallePicker, ColorPicker, FilePicker, Tab, };
+export { CustomButton, AIPicker, DallePicker, SDPicker, ColorPicker, FilePicker, Tab, };
