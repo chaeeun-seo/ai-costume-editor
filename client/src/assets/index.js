ec6e3a96b8f17dab452114c0c02ac63f9b8b47fd
chaeeun-seo <seochaeeun961126@gmail.com>
2023-10-30 11:34:42 +0900

    add stabilityAI in index.js to export from assets folder

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,6 +1,7 @@
 import ai from "./ai.png";
 import openai from "./openai.png";
-import stableDiffusion from "./stable-diffusion.png"; 
+import stableDiffusion from "./stable-diffusion.png";
+import stabilityAI from "./stabilityAI.png";
 import fileIcon from "./file.png";
 import swatch from "./swatch.png";
 import download from "./download.png";
@@ -8,4 +9,4 @@ import download from "./download.png";
 import logoShirt from "./logo-tshirt.png";
 import stylishShirt from "./stylish-tshirt.png";
 
-export { ai, openai, stableDiffusion, fileIcon, swatch, download, logoShirt, stylishShirt };
+export { ai, openai, stableDiffusion, stabilityAI, fileIcon, swatch, download, logoShirt, stylishShirt };
