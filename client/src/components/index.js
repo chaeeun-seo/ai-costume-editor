a673fd8e97afb68f15a74e2b1032aa2cefc25526
chaeeun-seo <seochaeeun961126@gmail.com>
2023-10-30 11:38:28 +0900

    export SAPicker from components folder

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,8 +2,9 @@ import CustomButton from "./CustomButton";
 import AIPicker from "./AIPicker";
 import DallePicker from "./DallePicker";
 import SDPicker from "./SDPicker";
+import SAPicker from "./SAPicker";
 import ColorPicker from "./ColorPicker";
 import FilePicker from "./FilePicker";
 import Tab from "./Tab";
 
-export { CustomButton, AIPicker, DallePicker, SDPicker, ColorPicker, FilePicker, Tab, };
+export { CustomButton, AIPicker, DallePicker, SDPicker, SAPicker, ColorPicker, FilePicker, Tab, };
