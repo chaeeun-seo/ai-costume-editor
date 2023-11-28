8c57851ad2ce5c4d0c4875db8b9c016f123b2289
chaeeun-seo <seochaeeun961126@gmail.com>
2023-11-28 10:14:43 +0900

    add NovitaPicker to index.js in components folder to export

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,8 +3,9 @@ import AIPicker from "./AIPicker";
 import DallePicker from "./DallePicker";
 import SDPicker from "./SDPicker";
 import SAPicker from "./SAPicker";
+import NovitaPicker from "./NovitaPicker";
 import ColorPicker from "./ColorPicker";
 import FilePicker from "./FilePicker";
 import Tab from "./Tab";
 
-export { CustomButton, AIPicker, DallePicker, SDPicker, SAPicker, ColorPicker, FilePicker, Tab, };
+export { CustomButton, AIPicker, DallePicker, SDPicker, SAPicker, NovitaPicker, ColorPicker, FilePicker, Tab, };
