7e2ac0c37885aea61f5ad51e581952ca49a9f598
chaeeun-seo <seochaeeun961126@gmail.com>
2023-11-28 10:12:10 +0900

    add novita logo to index.js in assets folder to export

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -2,6 +2,7 @@ import ai from "./ai.png";
 import openai from "./openai.png";
 import stableDiffusion from "./stable-diffusion.png";
 import stabilityAI from "./stabilityAI.png";
+import novita from "./novita.png";
 import fileIcon from "./file.png";
 import swatch from "./swatch.png";
 import download from "./download.png";
@@ -9,4 +10,4 @@ import download from "./download.png";
 import logoShirt from "./logo-tshirt.png";
 import stylishShirt from "./stylish-tshirt.png";
 
-export { ai, openai, stableDiffusion, stabilityAI, fileIcon, swatch, download, logoShirt, stylishShirt };
+export { ai, openai, stableDiffusion, stabilityAI, novita, fileIcon, swatch, download, logoShirt, stylishShirt };
