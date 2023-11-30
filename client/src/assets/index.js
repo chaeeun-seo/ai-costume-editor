4dee2b3e8af0150e8f214c4cab790ca6e6868535
chaeeun-seo <seochaeeun961126@gmail.com>
2023-11-30 15:18:59 +0900

    add sda logo to index.js in assets folder to export

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -3,6 +3,7 @@ import openai from "./openai.png";
 import stableDiffusion from "./stable-diffusion.png";
 import stabilityAI from "./stabilityAI.png";
 import novita from "./novita.png";
+import sda from "./sda.png";
 import fileIcon from "./file.png";
 import swatch from "./swatch.png";
 import download from "./download.png";
@@ -10,4 +11,4 @@ import download from "./download.png";
 import logoShirt from "./logo-tshirt.png";
 import stylishShirt from "./stylish-tshirt.png";
 
-export { ai, openai, stableDiffusion, stabilityAI, novita, fileIcon, swatch, download, logoShirt, stylishShirt };
+export { ai, openai, stableDiffusion, stabilityAI, novita, sda, fileIcon, swatch, download, logoShirt, stylishShirt };
