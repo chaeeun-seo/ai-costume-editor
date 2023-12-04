576e1dcf05cc54125f051cff7f62b85d8a741ad2
chaeeun-seo <seochaeeun961126@gmail.com>
2023-12-04 13:27:44 +0900

    make SDAPicker able to load models, delete models, get all models, create images based on models

--- client/src/components/SDAPicker.jsx
+++ client/src/components/SDAPicker.jsx
@@ -4,8 +4,103 @@ import { useSnapshot } from 'valtio';
 import CustomButton from './CustomButton';
 import state from '../store';
 
-const SDAPicker = ({ promptSDA, setPromptSDA, generatingImgSDA, handleSubmitSDA, handleDecals, imgSrcSDA, setImgSrcSDA, selectedImgSDA, setSelectedImgSDA }) => {
+const SDAPicker = ({ modelIdToGenerate, setModelIdToGenerate, promptSDA, setPromptSDA, generatingImgSDA, handleSubmitSDA, handleDecals, imgSrcSDA, setImgSrcSDA, selectedImgSDA, setSelectedImgSDA }) => {
   const snap = useSnapshot(state);
+  const SDA_ENTERPRISE_API_KEY = import.meta.env.VITE_SDA_ENTERPRISE_API_KEY;
+
+  const [modelList, setModelList] = useState(["test1", "test2", "test3"]);
+  const [modelUrl, setModelUrl] = useState("");
+  const [modelIdToLoad, setModelIdToLoad] = useState("");
+  const [modelType, setModelType] = useState("");
+
+  const getAllModels = async () => {
+    try {
+      const response = await fetch("https://stablediffusionapi.com/api/v1/enterprise/get_all_models", {
+          method: 'POST', 
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            "key": SDA_ENTERPRISE_API_KEY,
+          }),
+          redirect: 'follow',
+        });
+
+      const restext = await response.text();
+      const data = await JSON.parse(restext);
+      setModelList(data.models.map((model) => {
+        return model.model_id;
+      }));
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  const deleteModel = async (modelId) => {
+    try {
+      const response = await fetch('https://stablediffusionapi.com/api/v1/enterprise/delete_model', {
+          method: 'POST', 
+          headers: {
+            "Content-Type": "application/json",     
+          },
+          body: JSON.stringify({
+            "key": SDA_ENTERPRISE_API_KEY,
+            "model_id": modelId,
+          }),
+          redirect: 'follow',
+        });
+
+      const restext = await response.text();
+      const data = await JSON.parse(restext);
+      if (data.status == "success") {
+        alert(data.message);
+        getAllModels();
+      }
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  const loadModel = async () => {
+    try {
+      const response = await fetch("https://stablediffusionapi.com/api/v1/enterprise/load_model", {
+          method: 'POST', 
+          headers: {
+            "Content-Type": "application/json",     
+          },
+          body: JSON.stringify({
+            "key": SDA_ENTERPRISE_API_KEY,
+            "url": modelUrl,
+            "model_id": modelIdToLoad,
+            "model_type": modelType,
+            "from_safetensors": "yes",
+            "webhook": "https://stablediffusionapi.com",
+            "revision": "fp32",
+            "upcast_attention": "no"
+          }),
+          redirect: 'follow',
+        });
+
+      const restext = await response.text();
+      const data = await JSON.parse(restext);
+      if (data.status == "success") {
+        alert(data.message);
+        getAllModels();
+      }
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  const modelBox = modelList.map((modelId, idx) => {
+    return (
+      <div key={idx} className='flex justify-between'>
+        <span>{modelId}</span>
+        <button onClick={() => deleteModel(modelId)} className="cursor-pointer">X</button>
+      </div>
+    )
+  })
+
   const imageContainer = imgSrcSDA.map((img, index) => {
     return (
       <label key={index} className='cursor-pointer'>
@@ -32,6 +127,52 @@ const SDAPicker = ({ promptSDA, setPromptSDA, generatingImgSDA, handleSubmitSDA,
 
   return (
     <div className={`aipicker-container ${imgSrcSDA.length > 0 ? "w-[300px]" : "w-[195px]"}`}>
+      <h3 className='font-bold'>모델 조회하기</h3>
+      {modelBox}
+      <CustomButton 
+        type="filled"
+        title="모델 조회하기"
+        handleClick={() => getAllModels()}
+        customStyles="text-xs"
+      />
+
+      <h3 className='mt-4 font-bold'>모델 추가히기</h3>
+      <input 
+        placeholder="url"
+        // rows={1} 
+        value={modelUrl}
+        onChange={(e) => setModelUrl(e.target.value)} 
+        className="aipicker-textarea"
+      />
+      <input 
+        placeholder="model_id"
+        rows={1} 
+        value={modelIdToLoad}
+        onChange={(e) => setModelIdToLoad(e.target.value)} 
+        className="aipicker-textarea"
+      />
+      <input 
+        placeholder="model_type"
+        rows={1} 
+        value={modelType}
+        onChange={(e) => setModelType(e.target.value)} 
+        className="aipicker-textarea"
+      />
+      <CustomButton 
+        type="filled"
+        title="모델 추가하기"
+        handleClick={() => loadModel()}
+        customStyles="text-xs"
+      />
+
+      <h3 className='mt-4 font-bold'>이미지 생성하기</h3>
+      <select name="" id="" onChange={(e) => {
+          setModelIdToGenerate(e.target.value)
+      }}>
+        {modelList.map((model, idx) => {
+          return <option key={idx} value={model}>{model}</option>
+        })}
+      </select>
       <textarea 
         placeholder="Ask Stable diffusion API..."
         rows={5} 
